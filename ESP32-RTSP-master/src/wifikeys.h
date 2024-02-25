8bf5db3166e18504d7845082734b0fe7700a4d21
ShizzleBlitz <100959619+ShizzleBlitz@users.noreply.github.com>
2024-02-25 18:05:42 +0500

    Add files via upload

---
+++ ESP32-RTSP-master/src/wifikeys.h
@@ -0,0 +1,4 @@
+#include <Arduino.h>
+// Change YOUR_AP_NAME and YOUR_AP_PASSWORD to your WiFi credentials
+const char *ssid = "Galaxy A04s 2E2A";		   // Put your SSID here
+const char *password = "pbdq7975"; // Put your PASSWORD here
